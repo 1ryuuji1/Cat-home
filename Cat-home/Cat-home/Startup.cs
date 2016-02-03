94af2754daac580a0b83aac261f533adaf0af36b
yangqi <qyang@ideabinder.com>
2016-02-03 09:58:05 +0800

    创建项目

---
+++ Cat-home/Cat-home/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cat_home.Startup))]
+namespace Cat_home
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
